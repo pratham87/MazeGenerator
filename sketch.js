5e7629caf8df2859050425c48aae3cf685f95dda
Prathamesh Kawale <kawa5@live.com>
2016-12-18 00:49:01 -0800

    creating 2x2 matrix

--- sketch.js
+++ sketch.js
@@ -1,8 +1,36 @@
-function setup(){
+var rows, cols;
+var w = 40;
+var grid = [];
 
-createCanvas(400,400);
+function setup() {
+    createCanvas(400, 400);
+    cols = floor(width / w);
+    rows = floor(height / w);
+
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var cell = new Cell(i, j);
+            grid.push(cell);
+        }
+    }
 }
 
-function draw(){
-background(51);
+function draw() {
+    background(51);
+    for (var i = 0; i < grid.length; i++) {
+        grid[i].show();
+    }
+}
+
+function Cell(i, j) {
+    this.i = i;
+    this.j = j;
+
+    this.show = function show() {
+        var x = this.i * w;
+        var y = this.j * w;
+        stroke(255);
+        noFill();
+        rect(x, y, w, w);
+    }
 }
