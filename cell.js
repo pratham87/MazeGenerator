6d68ccbe652b38c134f05916670872f4aa0b3790
Prathamesh Kawale <kawa5@live.com>
2016-12-23 18:33:06 -0800

    implementing removeWalls separately

--- cell.js
+++ cell.js
@@ -67,23 +67,3 @@ function Cell(i, j) {
         }
     }
 }
-
-function removeWalls(a, b) {
-    var x = a.i - b.i;
-    if (x === 1) {
-        a.walls[3] = false;
-        b.walls[1] = false;
-    } else if (x === -1) {
-        a.walls[1] = false;
-        b.walls[3] = false;
-    }
-
-    var y = a.j - b.j;
-    if (y === 1) {
-        a.walls[0] = false;
-        b.walls[2] = false;
-    } else if (y === -1) {
-        a.walls[2] = false;
-        b.walls[0] = false;
-    }
-}
---
+++ removeWalls.js
@@ -0,0 +1,19 @@
+function removeWalls(a, b) {
+    var x = a.i - b.i;
+    if (x === 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+    } else if (x === -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+
+    var y = a.j - b.j;
+    if (y === 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+    } else if (y === -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
+    }
+}
